be358b82ea98d9b866ed7467d038ec1716454b5a
Diego Villodas Zapata <diego.villodas@sngular.com>
2024-03-09 16:44:32 +0100

    Add first UI layouts

---
+++ TravelMate/app/src/main/java/com/muei/travelmate/ui/home/Home.kt
@@ -0,0 +1,20 @@
+package com.muei.travelmate.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.muei.travelmate.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+//        val image2: ImageView = findViewById(R.id.image2)
+//        image2.setOnClickListener {
+//            val intent = Intent(this@Home, SearchView::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
---
+++ TravelMate/app/src/main/java/com/muei/travelmate/ui/home/SearchView.kt
@@ -0,0 +1,12 @@
+package com.muei.travelmate.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muei.travelmate.R
+
+class SearchView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_view)
+    }
+}
