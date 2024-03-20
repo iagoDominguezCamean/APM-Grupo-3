b4c81c0b5151cfdcd915a8eca1e9367dea172d06
iagoDominguezCamean <72920871+iagoDominguezCamean@users.noreply.github.com>
2024-03-20 19:47:16 +0100

    Update

--- TravelMate/app/src/main/java/com/muei/travelmate/ui/home/SearchFragment.kt
+++ TravelMate/app/src/main/java/com/muei/travelmate/ui/home/SearchFragment.kt
@@ -1,19 +1,19 @@
 package com.muei.travelmate.ui.home
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.muei.travelmate.R
-import com.muei.travelmate.databinding.FragmentHomeBinding
+import com.muei.travelmate.databinding.FragmentSearchBinding
 
-class HomeFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +25,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(SearchViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        binding.searchBar.setOnClickListener {
-            findNavController().navigate(R.id.nav_search)
-        }
-
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        binding.arrowIcon.setOnClickListener { findNavController().navigate(R.id.nav_home) }
         return binding.root
     }
 
