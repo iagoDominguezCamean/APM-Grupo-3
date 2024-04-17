8027140c30453d7a54eb10214e387fb67ee878bc
iagoDominguezCamean <72920871+iagoDominguezCamean@users.noreply.github.com>
2024-04-17 18:08:11 +0200

    Update: Request location permission

--- TravelMate/app/src/main/java/com/muei/travelmate/ui/MainActivity.kt
+++ TravelMate/app/src/main/java/com/muei/travelmate/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.muei.travelmate.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -38,7 +42,14 @@ class MainActivity : AppCompatActivity() {
 
                 R.id.nav_map -> {
                     Log.d("MainActivity", "Map pulsado")
-                    navController.navigate(R.id.nav_map)
+                    if(!checkPermission()){
+                        Log.d("Permisos de geolocalización", "Solicitando")
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+                    }else{
+                        Log.d("Permisos de geolocalización", "Concedidos")
+                        navController.navigate(R.id.nav_map)
+                    }
+
                     true
                 }
 
@@ -51,5 +62,12 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+    private fun checkPermission(): Boolean{
+        return (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    }
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
     }
 }
--- TravelMate/app/src/main/java/com/muei/travelmate/ui/home/MapFragment.kt
+++ TravelMate/app/src/main/java/com/muei/travelmate/ui/home/MapFragment.kt
@@ -1,23 +1,29 @@
 package com.muei.travelmate.ui.home
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.SupportMapFragment
 import com.muei.travelmate.R
 import com.muei.travelmate.databinding.FragmentMapBinding
-import com.muei.travelmate.databinding.FragmentUserBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
 
@@ -25,6 +31,7 @@ class MapFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,32 +52,45 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val mapView = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapView.getMapAsync {map ->
+        mapView.getMapAsync { map ->
             googleMap = map
-
-            googleMap.uiSettings.isZoomControlsEnabled = true
-            googleMap.setOnMapClickListener { latLng ->
-                // Handle map click events
-                // Example: Add a marker at the clicked position
-                googleMap.addMarker(MarkerOptions().position(latLng).title("Clicked Position"))
-            }
-
-            // Move camera to a default location (for example, city center)
-            val defaultLocation = LatLng(DEFAULT_LATITUDE, DEFAULT_LONGITUDE)
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM_LEVEL))
         }
-
-
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+    override fun onMapReady(map: GoogleMap){
+        googleMap = map
+
+        if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        googleMap.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null){
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                googleMap.addMarker(MarkerOptions().position(currentLatLng).title("Current Position"))
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, DEFAULT_ZOOM_LEVEL))
+            }else{
+                Toast.makeText(requireContext(), "Unable to get current location", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
     companion object {
         private const val DEFAULT_LATITUDE = -33.8688
         private const val DEFAULT_LONGITUDE = 151.2093
         private const val DEFAULT_ZOOM_LEVEL = 12f
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
     }
 }
