5249a90577e965b16195d718cda5c56fba27ed67
Pablo Mariño Boga <pablo.marino.boga@gmail.com>
2024-04-21 00:00:54 +0200

    Adds token info to user section

--- TravelMate/app/src/main/java/com/muei/travelmate/ui/home/UserFragment.kt
+++ TravelMate/app/src/main/java/com/muei/travelmate/ui/home/UserFragment.kt
@@ -1,12 +1,18 @@
 package com.muei.travelmate.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.muei.travelmate.databinding.FragmentUserBinding
+import com.muei.travelmate.ui.auth.User
+import com.muei.travelmate.ui.image.CircleTransform
+import com.squareup.picasso.Picasso
 
 
 class UserFragment : Fragment() {
@@ -27,11 +33,40 @@ class UserFragment : Fragment() {
 
         _binding = FragmentUserBinding.inflate(inflater, container, false)
 
+
+        val idToken = readFromSharedPreferences(requireContext(), "id_token", "")
+        // If there is a JWT id stored
+        if(idToken!=""){
+            // check if the token has expired
+            val user = User(idToken)
+            val userName : TextView = binding.root.findViewById(com.muei.travelmate.R.id.userName)
+            userName.text=user.name
+            val userEmail : TextView = binding.root.findViewById(com.muei.travelmate.R.id.userMail)
+            userEmail.text=user.email
+            val imgView: ImageView = binding.root.findViewById(com.muei.travelmate.R.id.imageView2)
+            Picasso.get().load(user.picture).transform(CircleTransform()).into(imgView)
+
+
+            //cargarImagenUsuario( binding.root.findViewById(R.id.imageView2),user.picture)
+
+            // Glide
+
+
+
+
+
+
+        }
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    private fun readFromSharedPreferences(context: Context, key: String, defaultValue: String): String {
+        val sharedPref = context.getSharedPreferences("travelmate_prefs", Context.MODE_PRIVATE)
+        return sharedPref.getString(key, defaultValue) ?: defaultValue
     }
 }
